9db6d3fc10bb8e2e0d3e672aa5aa058a17761c4b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-03-06 14:37:25 +0000

    Section 4.5 --> 4.7 (Lishkov substitution)
    Have quantity return a consistent
    object type (String) so sender does
    not need to know about receiver.
    
    It returns what it promises, a string,
    not a string OR a number.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,18 +11,26 @@ class Bottles
     case number
     when 0
       <<~VERSE
-        No more bottles of beer on the wall, no more bottles of beer.
+        #{quantity(number).capitalize} #{container(number)} of beer on the wall, no more bottles of beer.
         Go to the store and buy some more, 99 bottles of beer on the wall.
       VERSE
     else
       <<~VERSE
-        #{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.
+        #{quantity(number).capitalize} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.
         Take #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number-1)} of beer on the wall.
       VERSE
     end
   end
 
   private
+
+  def quantity(number)
+    if number == 0
+      'no more'
+    else
+      number.to_s
+    end
+  end
 
   def container(number)
     if number.eql?(1)
@@ -37,14 +45,6 @@ class Bottles
       'it'
     else
       'one'
-    end
-  end
-
-  def quantity(number)
-    if number == 0
-      'no more'
-    else
-      number
     end
   end
 end
